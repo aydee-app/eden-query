4df846fe060d2960e3e6744cf7bf25a85ab337ec
Aponia <ap0nia.dev@gmail.com>
2025-03-03 12:20:36 -0800

    test(svelte-query): 🚨 createMutation initial test suite

--- packages/eden-svelte-query/tests/implementation/treaty/createMutation.test.ts
+++ packages/eden-svelte-query/tests/implementation/treaty/createMutation.test.ts
@@ -1,56 +1,195 @@
 import { EdenClient, httpLink } from '@ap0nia/eden'
 import { QueryClient } from '@tanstack/svelte-query'
-import { get, writable } from 'svelte/store'
-import { describe, expect, test } from 'vitest'
+import { get, readable, writable } from 'svelte/store'
+import { describe, expect, test, vi } from 'vitest'
 
-import { eden } from '../../eden'
+import type { EdenCreateMutationResult } from '../../../src/integration/hooks/create-mutation'
+import { createEden, eden } from '../../eden'
+
+const queryClient = new QueryClient()
+
+const client = new EdenClient({
+  links: [httpLink()],
+})
+
+const context = eden.createContext({ queryClient, client })
 
 describe('createMutation', () => {
-  test('substitutes object path params', async () => {
-    const queryClient = new QueryClient()
+  describe('hook is called with correct arguments', () => {
+    test('option and path params are not readable, resolved arguments are not readable', async () => {
+      const id = 'ID'
 
-    const client = new EdenClient({
-      links: [httpLink()],
+      const rootHooks = {
+        createMutation: vi.fn(),
+      }
+
+      const eden = createEden(undefined, rootHooks as any)
+
+      eden.posts({ id }).post.createMutation(undefined)
+
+      const paths = ['posts', ':id', 'post']
+
+      const options = { params: { id } }
+
+      expect(rootHooks.createMutation).toHaveBeenCalledWith(paths, options)
     })
 
-    const context = eden.createContext({ queryClient, client })
+    test('options is not readable, path params is readable, resolved options is readable', async () => {
+      const id = 'ID'
 
-    const id = 'ID'
+      let args: any[] = []
 
-    const mutation = eden.posts({ id }).post.createMutation(undefined, context)
+      const rootHooks = {
+        createMutation: vi.fn((...createMutationArgs) => (args = createMutationArgs)),
+      }
 
-    const $mutation = get(mutation)
+      const eden = createEden(undefined, rootHooks as any)
 
-    // This should be the ID that was submitted.
-    const result = await $mutation.mutateAsync({ message: 'Hello' })
+      eden.posts(readable({ id })).post.createMutation(undefined)
 
-    expect(result).toBe(id)
+      const paths = ['posts', ':id', 'post']
+
+      expect(rootHooks.createMutation).toHaveBeenCalledWith(
+        paths,
+        expect.objectContaining({ subscribe: expect.anything() }),
+      )
+
+      expect(args[1]).toHaveProperty('subscribe')
+
+      const params = get(args[1])
+
+      expect(params).toStrictEqual({ params: { id } })
+    })
+
+    test('options is readable, path params is not readable, resolved options is readable', async () => {
+      const id = 'ID'
+
+      let args: any[] = []
+
+      const rootHooks = {
+        createMutation: vi.fn((...createMutationArgs) => (args = createMutationArgs)),
+      }
+
+      const eden = createEden(undefined, rootHooks as any)
+
+      eden.posts({ id }).post.createMutation(readable(undefined))
+
+      const paths = ['posts', ':id', 'post']
+
+      expect(rootHooks.createMutation).toHaveBeenCalledWith(
+        paths,
+        expect.objectContaining({ subscribe: expect.anything() }),
+      )
+
+      expect(args[1]).toHaveProperty('subscribe')
+
+      const params = get(args[1])
+
+      expect(params).toStrictEqual({ params: { id } })
+    })
   })
 
-  test('substitutes readable path params', async () => {
-    const queryClient = new QueryClient()
+  describe('path parameters', () => {
+    test('substitutes object path params and requests correct URL', async () => {
+      const id = 'ID'
 
-    const client = new EdenClient({
-      links: [httpLink()],
+      const fetcher = vi.fn(async () => {
+        return Response.json('OK')
+      })
+
+      const mutation = eden.posts({ id }).post.createMutation({ eden: { fetcher } }, context)
+
+      const $mutation = get(mutation)
+
+      await $mutation.mutateAsync({ message: 'Hello' })
+
+      expect(fetcher).toHaveBeenCalledWith('http://localhost:3001/posts/ID', expect.anything())
     })
 
-    const context = eden.createContext({ queryClient, client })
+    test('substitutes readable path params and requests correct URL', async () => {
+      const id = writable({ id: 'ID' })
 
-    const id = writable({ id: 'ID' })
+      const fetcher = vi.fn(async () => {
+        return Response.json('OK')
+      })
 
-    const mutation = eden.posts(id).post.createMutation(undefined, context)
+      const mutation = eden.posts(id).post.createMutation({ eden: { fetcher } }, context)
 
-    const $mutation = get(mutation)
+      const $mutation = get(mutation)
 
-    // This should be the ID that was submitted.
-    const result = await $mutation.mutateAsync({ message: 'Hello' })
+      await $mutation.mutateAsync({ message: 'Hello' })
 
-    expect(result).toBe(get(id).id)
+      expect(fetcher).toHaveBeenCalledWith('http://localhost:3001/posts/ID', expect.anything())
 
-    id.set({ id: 'ID2' })
+      id.set({ id: 'ID2' })
 
-    const result2 = await $mutation.mutateAsync({ message: 'Hello' })
+      await $mutation.mutateAsync({ message: 'Hello' })
 
-    expect(result2).toBe(get(id).id)
+      expect(fetcher).toHaveBeenCalledWith('http://localhost:3001/posts/ID2', expect.anything())
+    })
+  })
+
+  describe('mutation options', () => {
+    describe('onMutate', () => {
+      test('forwards context from onMutate to onSuccess callback', async () => {
+        const id = 'ID'
+
+        const body = { message: 'Hello' }
+
+        const mutationContext = 'HI'
+
+        const onSuccess = vi.fn()
+
+        const onMutate = vi.fn((_variables) => {
+          return mutationContext
+        })
+
+        const mutation = eden.posts({ id }).post.createMutation({ onSuccess, onMutate }, context)
+
+        const $mutation = get(mutation)
+
+        await $mutation.mutateAsync(body)
+
+        expect(onMutate).toHaveBeenCalledWith(expect.objectContaining({ body }))
+
+        expect(onSuccess).toHaveBeenCalledWith(
+          expect.anything(),
+          expect.anything(),
+          mutationContext,
+        )
+      })
+    })
+
+    test('calls on success for valid request', async () => {
+      const body = { message: 'hello' }
+
+      const onSuccess = vi.fn()
+
+      const mutation = eden.posts({ id: 1 }).post.createMutation({ onSuccess }, context)
+
+      const $mutation = get(mutation)
+
+      await $mutation.mutateAsync(body)
+
+      expect(onSuccess).toHaveBeenCalledOnce()
+
+      expect(onSuccess).toHaveBeenCalledWith(expect.anything(), expect.anything(), undefined)
+    })
+
+    test('calls on onError for invalid request', async () => {
+      const onError = vi.fn()
+
+      // @ts-expect-error Testing invalid route.
+      const mutation = eden.posts.invalid.post.createMutation(
+        { onError },
+        context,
+      ) as EdenCreateMutationResult<any, any, {}, any, any>
+
+      const $mutation = get(mutation)
+
+      await expect($mutation.mutateAsync()).rejects.toThrowError()
+
+      expect(onError).toHaveBeenCalledWith(expect.anything(), expect.anything(), undefined)
+    })
   })
 })
