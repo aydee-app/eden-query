7558f3d26b74bdf00eb89f3afa19ca0a320d1055
Aponia <ap0nia.dev@gmail.com>
2025-03-03 12:18:22 -0800

    test(svelte-query): 🚨 added createEden utility for dynamic client initialization

--- packages/eden-svelte-query/tests/eden.ts
+++ packages/eden-svelte-query/tests/eden.ts
@@ -1,6 +1,16 @@
 import { createEdenTreatySvelteQuery } from '../src'
 import type { App } from './app'
 
+export function createEden(...args: Parameters<typeof createEdenTreatySvelteQuery>) {
+  return createEdenTreatySvelteQuery<App>(
+    {
+      domain: 'http://localhost:3001',
+      ...args[0],
+    },
+    args[1] as any,
+  )
+}
+
 export const eden = createEdenTreatySvelteQuery<App>({
   domain: 'http://localhost:3001',
 })
