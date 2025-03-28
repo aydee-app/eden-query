8348323aa0675174dd2696179a7fe2c6b4a4624f
Aponia <ap0nia.dev@gmail.com>
2025-03-28 14:23:31 -0700

    refactor(eden2): ♻️ loosen signature of inputWithTrackedEventId

--- packages/eden2/src/links/internal/input-with-tracked-event-id.ts
+++ packages/eden2/src/links/internal/input-with-tracked-event-id.ts
@@ -1,4 +1,7 @@
-export function inputWithTrackedEventId(input: unknown, lastEventId: string | undefined) {
+export function inputWithTrackedEventId(
+  input: unknown,
+  lastEventId?: string | undefined | null | void,
+) {
   if (!lastEventId) return input
 
   if (input != null && typeof input !== 'object') return input
