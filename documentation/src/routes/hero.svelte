a6edfd4c48b28d97e917c6b96e4ce0ccefa40830
Aponia <ap0nia.dev@gmail.com>
2025-05-08 14:09:46 -0700

    feat(docs): ✨ 📚️ index page mvp

---
+++ documentation/src/routes/hero.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  import { cn } from '$lib/utils/cn'
+
+  import Ray from './ray.svelte'
+
+  interface $$Props {
+    children?: Snippet
+  }
+
+  const { children }: $$Props = $props()
+
+  const value = 'bun create elysia app'
+
+  let copied = $state(false)
+
+  let timeout = $state<ReturnType<typeof setTimeout>>()
+
+  async function handleCopy(e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
+    if (typeof navigator === 'undefined') return
+
+    const text = value
+
+    if (text == null) return
+
+    e.preventDefault()
+    e.stopPropagation()
+
+    await navigator.clipboard.writeText(text)
+
+    copied = true
+
+    debouncedResetCopyCode()
+  }
+
+  function debouncedResetCopyCode() {
+    clearTimeout(timeout)
+    timeout = setTimeout(resetCopyCode, 1_000)
+  }
+
+  function resetCopyCode() {
+    copied = false
+  }
+</script>
+
+<div class="flex w-full flex-col gap-12" style="min-height: calc(100dvh - var(--rp-nav-height))">
+  <Ray class="pointer-events-none -top-16 h-[60vh] opacity-[.35] dark:opacity-50" />
+
+  <div class="flex w-full grow flex-col items-center justify-center gap-8 p-4">
+    <div
+      id="splash"
+      class="gradient pointer-events-none absolute top-[-70vh] block h-screen w-full max-w-full justify-center opacity-25"
+    ></div>
+
+    <img
+      src="/assets/elysia_v.webp"
+      alt="Curved text logo saying 'Elysia JS'"
+      class="aspect-3/2 w-full max-w-md object-contain"
+    />
+
+    <p>
+      <span
+        class="bg-gradient-to-r from-sky-300 to-violet-400 bg-clip-text text-5xl font-bold leading-tight text-transparent md:text-center md:text-6xl"
+      >
+        Ergonomic Framework for Humans
+      </span>
+
+      <span class="icon-[ph--sparkle-fill] h-10 w-10 align-top text-indigo-400"></span>
+    </p>
+
+    <h3
+      class="w-full max-w-2xl text-xl !leading-normal text-gray-500 md:text-center md:text-2xl dark:text-gray-400"
+    >
+      <span>TypeScript with &nbsp;</span>
+
+      <span
+        class="bg-gradient-to-r from-indigo-400 to-purple-400 bg-clip-text font-semibold text-transparent"
+      >
+        End-to-End Type Safety
+      </span>
+
+      <span>, type integrity, and exceptional developer experience. Supercharged by Bun.</span>
+    </h3>
+
+    <section class="flex flex-wrap items-center gap-4 md:justify-center">
+      <a class="btn btn-primary rounded-full px-6 text-lg" href="/eden-query/index">
+        Get Started
+      </a>
+
+      <div class="flex gap-4">
+        <code
+          class="bg-primary/25 whitespace-nowrap rounded-full px-6 py-2.5 font-mono text-lg font-medium"
+        >
+          {value}
+        </code>
+
+        <div
+          class={cn('tooltip-bottom tooltip-primary', copied && 'tooltip tooltip-open')}
+          data-tip="Copied"
+        >
+          <button
+            class={cn('swap btn btn-primary btn-outline', copied && 'swap-active')}
+            onclick={handleCopy}
+            aria-label="Copy start script"
+          >
+            <span class="swap-on icon-[material-symbols--check-rounded] h-6 w-6"></span>
+            <span class="swap-off icon-[material-symbols--content-copy-outline-rounded] h-6 w-6"
+            ></span>
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  {@render children?.()}
+</div>
---
+++ documentation/src/routes/ray.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { cn } from '$lib/utils/cn'
+
+  interface $$Props {
+    isSafari?: boolean
+    animated?: boolean
+    isStatic?: boolean
+    class?: string
+  }
+
+  const { isStatic, isSafari, animated, class: className }: $$Props = $props()
+
+  const shouldAnimate =
+    animated || (typeof navigator !== 'undefined' && navigator?.hardwareConcurrency > 4)
+</script>
+
+<div
+  class={cn(
+    'absolute left-0 top-0 z-[40]',
+    'h-[220px] w-full !max-w-full',
+    'flex flex-col items-center justify-center',
+    'transition-bg pointer-events-none overflow-hidden bg-transparent opacity-25 dark:opacity-[.55]',
+    className,
+  )}
+>
+  <div
+    class={cn(
+      'jumbo static absolute opacity-60',
+      isSafari && '-safari',
+      isStatic && '-static',
+      shouldAnimate && '-animate',
+    )}
+  ></div>
+</div>
