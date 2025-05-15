4b79d74344430baee19e2477a771a8aad2bea0dd
Aponia <ap0nia.dev@gmail.com>
2025-05-15 11:36:52 -0700

    fix: 🐛 build

---
+++ packages/svelte-query/rslib.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+import { defineConfig } from '@rslib/core'
+
+const config = defineConfig({
+  output: {
+    cleanDistPath: true,
+    distPath: {
+      root: 'dist',
+    },
+    sourceMap: true,
+  },
+  source: {
+    tsconfigPath: './tsconfig.build.json',
+  },
+  lib: [
+    {
+      bundle: true,
+      source: {
+        entry: {
+          index: './src/index.ts',
+        },
+      },
+      dts: true,
+      format: 'esm',
+    },
+    {
+      bundle: true,
+      source: {
+        entry: {
+          index: './src/index.ts',
+        },
+      },
+      dts: true,
+      format: 'cjs',
+    },
+  ],
+})
+
+export default config
